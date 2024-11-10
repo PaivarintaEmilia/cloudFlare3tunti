0ce15551b6d5a7be6cfc56ded57f2bdb62dbe281
Maria Emilia Päivärinta <eemilia.paivarinta@gmail.com>
2024-11-10 19:49:37 +0100

    add some testing changes

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,16 @@ app.get('/', (c) => {
   return c.text('Hello Cloudflare workers!')
 })
 
+app.get('/home', (c)=> {
+  return c.html(`<html>
+    
+    <body>
+
+    <h1>Muutokset Pipelinen kautta.</h1>
+
+    </body>
+
+    </html>`)
+})
+
 export default app
